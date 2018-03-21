a25d62165b6d5e16d664998832a2203ccc470534
jaceyli <ljingxuan@gmail.com>
2018-03-21 18:18:49 +1100

    add gitignore

--- Lab/Lab2/run.py
+++ Lab/Lab2/run.py
@@ -1,9 +1,3 @@
-# import module_1
-# from flask import Flask
-#
-# module_1 = Flask(__name__)
-#
-# # module_1.config.from_object('./config')
 from __init__ import module_1
-# module_1.config.from_object('./config')
+
 module_1.run()
